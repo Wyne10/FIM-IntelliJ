9f7c5808df48efe3eba04bbcd48aacc773917d08
ACraftingFish <agarcraftfish@gmail.com>
2025-07-14 16:03:42 +0200

    + Added a check for the runtime classtype for minimessage triggering, alongside declared classtypes

--- src/main/kotlin/fish/crafting/fimplugin/plugin/minimessage/MiniMessagePsiUtils.kt
+++ src/main/kotlin/fish/crafting/fimplugin/plugin/minimessage/MiniMessagePsiUtils.kt
@@ -6,26 +6,33 @@ import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.ReadAction
 import com.intellij.openapi.project.DumbService
 import com.intellij.psi.JavaRecursiveElementVisitor
+import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiExpression
 import com.intellij.psi.PsiExpressionList
 import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiLiteralExpression
 import com.intellij.psi.PsiMethod
 import com.intellij.psi.PsiMethodCallExpression
 import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.psi.util.PsiUtil
 import com.intellij.util.concurrency.AppExecutorUtil
 import fish.crafting.fimplugin.plugin.util.javakotlin.isJava
 import fish.crafting.fimplugin.plugin.util.javakotlin.isJson
 import fish.crafting.fimplugin.plugin.util.javakotlin.isKotlin
 import fish.crafting.fimplugin.plugin.util.javakotlin.isYaml
+import io.ktor.util.reflect.instanceOf
+import org.jetbrains.kotlin.idea.caches.resolve.analyze
 import org.jetbrains.kotlin.js.parser.sourcemaps.JsonString
 import org.jetbrains.kotlin.psi.KtCallExpression
 import org.jetbrains.kotlin.psi.KtStringTemplateExpression
 import org.jetbrains.kotlin.psi.KtTreeVisitorVoid
 import org.jetbrains.kotlin.psi.KtValueArgument
 import org.jetbrains.kotlin.psi.KtValueArgumentList
+import org.jetbrains.kotlin.psi.psiUtil.getQualifiedExpressionForSelector
 import org.jetbrains.uast.UCallExpression
 import org.jetbrains.uast.toUElement
+import org.jetbrains.uast.toUElementOfType
 import org.jetbrains.yaml.psi.YAMLQuotedText
 import org.jetbrains.yaml.psi.YamlRecursivePsiElementVisitor
 
@@ -97,6 +104,21 @@ fun PsiFile.visitExpressions(run: (PsiElement) -> Unit) {
     }
 }
 
+private fun PsiElement.getRuntimeContainingClass(): PsiClass? {
+    var callExpression: UCallExpression? = null
+    if(this.language.isJava){
+        val exprList = parent as? PsiExpressionList ?: return null
+        callExpression = (exprList.parent as? PsiMethodCallExpression)?.toUElementOfType()
+    }else if(this.language.isKotlin){
+        val valArg = parent as? KtValueArgument ?: return null
+        val exprList = valArg.parent as? KtValueArgumentList ?: return null
+        callExpression = (exprList.parent as? KtCallExpression)?.toUElementOfType()
+    }
+
+    if(callExpression == null) return null
+    return PsiUtil.resolveClassInType(callExpression.receiverType)
+}
+
 private fun PsiElement.resolveMethodFromLiteral(): PsiMethod? {
     if(this.language.isJava){
         val exprList = parent as? PsiExpressionList ?: return null
@@ -137,6 +159,12 @@ fun PsiElement.shouldFormatMCText(): Boolean {
     val method = resolveMethodFromLiteral() ?: return false
     if(TextFormatRegistryService.instance.isMethodValid(method)) return true
 
-    val containingClass = method.containingClass ?: return false
-    return TextFormatRegistryService.instance.isClassValid(containingClass)
+    val containingClass = method.containingClass
+    containingClass ?: return false
+
+    val declaredClassValid = TextFormatRegistryService.instance.isClassValid(containingClass)
+    if(declaredClassValid) return true
+
+    val runtimeContainingClass = getRuntimeContainingClass() ?: return false
+    return TextFormatRegistryService.instance.isClassValid(runtimeContainingClass)
 }
